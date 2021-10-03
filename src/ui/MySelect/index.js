0b358a1db73a6b5673d14ca8085a7a80ab74131b
yulian <yulik19971997@gmail.com>
2021-10-03 19:16:24 +0300

    add options for Select

--- src/ui/MySelect/index.js
+++ src/ui/MySelect/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import Select from 'react-select'
 
 const options = [
-	{ value: 'chocolate', label: 'Chocolate' },
-	{ value: 'strawberry', label: 'Strawberry' },
-	{ value: 'vanilla', label: 'Vanilla' }
+	{ value: 'action-and-adventure', label: 'Action & Adventure' },
+	{ value: 'drama', label: 'Drama' },
+	{ value: 'biography', label: 'Biography' },
+	{ value: 'music', label: 'Music' },
+	{ value: 'oscar', label: 'Oscar winning Movie' }
 ]
 
 function MySelect({ placeholder, type }) {
